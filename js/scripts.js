e3bae13ee79a41f9ee4b897282104c4095c75a37
enock24 <enookimuu@gmail.com>
2020-10-11 13:47:58 +0300

    add styles to css

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,33 @@
 $(document).ready(function(){
-    $("#pic1").click(function(){
-        $("#pic1").hide();
+    $("#image1").click(function(){
+        $("#image1").hide();
         $(".design").show();
     });
     $(".design").click(function(){
         $(".design").hide();
-        $("#pic1").show();
+        $("#image1").show();
     });
 });
 
  $(document).ready(function(){
-    $("#pic2").click(function(){
-        $("#pic2").hide();
+    $("#image2").click(function(){
+        $("#image2").hide();
         $(".development").show();
     });
     $(".development").click(function(){
         $(".development").hide();
-        $("#pic2").show();
+        $("#image2").show();
     });
 });
 
 $(document).ready(function(){
-    $("#pic3").click(function(){
-        $("#pic3").hide();
+    $("#image3").click(function(){
+        $("#image3").hide();
         $(".product").show();
     });
     $(".product").click(function(){
         $(".product").hide();
-        $("#pic3").show();
+        $("#image3").show();
     });
 });
 
